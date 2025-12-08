b1aacf4db424a5b6f36d03381b46e4b6da115dba
Harsh Patel <code.by.hp@gmail.com>
2025-12-08 09:08:39 +0530

    set venv visa

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Harsh Patel",
+    author_email="code.by.hp@gmail.com",
+    packages=find_packages()
+)
